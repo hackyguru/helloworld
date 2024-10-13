48118bd7a7652a27e36227dce49e29234dc86867
Guru <19eucs071@skcet.ac.in>
2024-10-13 15:57:21 +0530

    Update backup.js

--- backup.js
+++ backup.js
@@ -1,11 +1,16 @@
 const lighthouse = require('@lighthouse-web3/sdk');
+const path = require('path');
 
 async function main() {
+  // Get the current working directory (root of the repository)
+  const repoRoot = process.cwd();
 
   const uploadResponse = await lighthouse.upload(
-  './', 
-  '2fc357f7.f952020a20bd4e97afa7aca88221116d')
-  console.log(uploadResponse)
+    repoRoot, 
+    '2fc357f7.f952020a20bd4e97afa7aca88221116d'
+  );
+
+  console.log(uploadResponse);
   const cid = uploadResponse.data.Hash;
   console.log(`Uploaded to IPFS : ${cid}`);
   process.stdout.write(`::set-output name=cid::${cid}`);
